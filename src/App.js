87c5debde98b2f67fcd9e4f0b659d0d00e9f2348
siam1131 <siam.hasan@georgebrown.ca>
2024-10-12 00:55:47 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101464651 || Siam Hasan || DevOps</h5>
+      <h5>Developed By: 101464651 || Siam Hasan || DevOps - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
